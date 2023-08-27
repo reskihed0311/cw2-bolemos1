d3e349d0c3552386bce7d5870d5528e2f078e9a5
NotTrashraD <reski16@outlook.fr>
2023-08-27 21:38:59 +0200

    added hk416 base files

---
+++ lua/weapons/cw_rshd_hk416d/sh_sounds.lua
@@ -0,0 +1,8 @@
+CustomizableWeaponry:addFireSound("CW_HK416D_FIRE", "weapons/cw_mp5/mp5_fire.wav", 1, 113, CHAN_WEAPON)
+CustomizableWeaponry:addFireSound("CW_HK416D_FIRE_SUPPRESSED", "weapons/cw_mp5/mp5_fire_suppressed.wav", 1, 90, CHAN_WEAPON)
+
+
+
+CustomizableWeaponry:addReloadSound("CW_HK416D_MAGOUT", "weapons/cw_rshd_hk416d/magout.ogg")
+CustomizableWeaponry:addReloadSound("CW_HK416D_MAGIN", "weapons/cw_rshd_hk416d/magin.ogg")
+CustomizableWeaponry:addReloadSound("CW_HK416D_BOLTFORWARD", "weapons/cw_rshd_hk416d/boltdrop.ogg")
